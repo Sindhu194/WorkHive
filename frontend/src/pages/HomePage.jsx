3b95a519f7d0b3fb2453e5f859e518a367bca208
Sindhu194 <sindhuveerathu@gmail.com>
2025-02-22 18:11:34 +0530

    Modified the home page with new functionalities of recommending users on the side bar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../lib/axios";
 import Sidebar from "../components/Sidebar.jsx";
- import PostCreation from "../components/PostCreation.jsx";
- //import Post from "../components/Post.jsx";
+import PostCreation from "../components/PostCreation.jsx";
+import Post from "../components/Post.jsx";
 import { Users } from "lucide-react";
-//import RecommendedUser from "../components/RecommendedUser.jsx";
+import RecommendedUser from "../components/RecommendedUser.jsx";
 
 const HomePage = () => {
 	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
@@ -35,22 +35,32 @@ const HomePage = () => {
 			 <div className='col-span-1 lg:col-span-2 order-first lg:order-none'>
 				<PostCreation user={authUser} />
 
-				{/* {posts?.map((post) => (
+				{posts?.map((post) => (
 					<Post key={post._id} post={post} />
 				))}
 
-				{posts?.length === 0 && (
+{posts?.length === 0 && (
 					<div className='bg-white rounded-lg shadow p-8 text-center'>
 						<div className='mb-6'>
-							<Users size={64} className='mx-auto text-blue-500' />
+							<Users size={64} className='mx-auto text-orange-500' />
 						</div>
 						<h2 className='text-2xl font-bold mb-4 text-gray-800'>No Posts Yet</h2>
 						<p className='text-gray-600 mb-6'>Connect with others to start seeing posts in your feed!</p>
 					</div>
-				)} */}
+				)}
 			</div>
 
-	</div>
+			{recommendedUsers?.length > 0 && (
+				<div className='col-span-1 lg:col-span-1 hidden lg:block'>
+					<div className='bg-secondary rounded-lg shadow p-4'>
+						<h2 className='font-semibold mb-4'>People you may know</h2>
+						{recommendedUsers?.map((user) => (
+							<RecommendedUser key={user._id} user={user} />
+						))}
+					</div>
+				</div>
+			)}
+			</div>
 	);
 };
 export default HomePage;
