b366848c684be641e8fb460bf93ef63958a6d4b4
Sindhu194 <sindhuveerathu@gmail.com>
2025-02-23 12:30:05 +0530

    finished up with the final code fixes and am currently working on deploying the site

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,18 @@ import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 import connectionRoutes from "./routes/connection.route.js";
 import {connectDB} from "./lib/db.js";
+import path from "path";
 dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT ||5000;
-
+const __dirmane=path.resolve();
+if(process.env.NODE_ENV !=="production"){
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true,
 }));
+}
 app.use(cookieParser());
 app.use(express.json({limit:"5mb"}));
 
@@ -25,6 +28,14 @@ app.use("/api/v1/users",userRoutes);
 app.use("/api/v1/posts",postRoutes);
 app.use("/api/v1/notifications",notificationRoutes);
 app.use("/api/v1/connections",connectionRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirmane,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirmane,"frontend","dist","index.html"));
+});
+}
+
 
 
 app.listen(PORT,() =>{
